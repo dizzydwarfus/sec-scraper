5d001821c3ae58f9940261d154e4e83b9e47e74b
dizzydwarfus <67429119+dizzydwarfus@users.noreply.github.com>
2024-03-02 16:43:50 +0100

    changes in docker file, changes in structure of files

--- utils/_mapping.py
+++ utils/_mapping.py
@@ -1,310 +1,233 @@
 STANDARD_NAME_MAPPING = {
-    'Revenue':
-        [
-            'Revenue from Contract with Customer, Excluding Assessed Tax',
-            'Revenues',
-            'Revenue, Net',
-            'Sales Revenue Net',
-            'Sales Revenue, Net',],
-    'Cost of Goods Sold':
-        [
-            'Cost of Goods Sold',
-            'Cost Of Goods Sold',
-        ],
-    'Cost of Services':
-        [
-            'Cost of Services',
-            'Cost Of Services',
-        ],
-    'Cost of Revenue':
-        [
-            'Cost of sales',
-            'Cost of Revenue',
-            'Cost Of Revenue',
-            'Cost of Goods and Services Sold',
-            'Cost Of Goods And Services Sold',
-        ],
-    'Gross Profit':
-        [
-            '',
-        ],
-    'Operating Expenses':
-        [
-            'Operating Expenses',
-        ],
-    'Operating Income':
-        [
-            '',
-        ],
-    'Interest Expense':
-        [
-            '',
-        ],
-    'Income Before Tax':
-        [
-            '',
-        ],
-    'Income Tax Expense':
-        [
-            'Income Taxes Paid, Net',
-            'Income Taxes Paid Net'
-        ],
-    'Net Income':
-        [
-            'Net Income (Loss)',
-            'Net income',
-            'Net sales',
-            'Net Income Loss',
-        ],
-    'Cash and Cash Equivalents':
-        [
-            '',
-        ],
-    'Short Term Investments':
-        [
-            '',
-        ],
-    'Total Cash':
-        [
-            '',
-        ],
-    'Accounts Receivable':
-        [
-            'Accounts Receivable Net Current',
-            'Accounts Receivable, Net, Current',
-        ],
-    'Accounts Payable':
-        [
-            'Accounts Payable Current',
-            'Accounts Payable, Current'
-        ],
-    'Net Receivables':
-        [
-            '',
-        ],
-    'Inventory':
-        [
-            '',
-        ],
-    'Other Current Assets':
-        [
-            '',
-        ],
-    'Total Current Assets':
-        [
-            '',
-        ],
-    'Long Term Investments':
-        [
-            '',
-        ],
-    'Property Plant and Equipment':
-        [
-            '',
-        ],
-    'Goodwill':
-        [
-            '',
-        ],
-    'Intangible Assets':
-        [
-            '',
-        ],
-    'Other Assets':
-        [
-            '',
-        ],
-    'Total Assets':
-        [
-            '',
-        ],
-    'Short/Current Long Term Debt':
-        [
-            '',
-        ],
-    'Other Current Liabilities':
-        [
-            '',
-        ],
-    'Total Current Liabilities':
-        [
-            '',
-        ],
-    'Long Term Debt':
-        [
-            '',
-        ],
-    'Other Liabilities':
-        [
-            '',
-        ],
-    'Total Liabilities':
-        [
-            '',
-        ],
-    'Common Stock':
-        [
-            '',
-        ],
-    'Retained Earnings':
-        [
-            '',
-        ],
-    'Treasury Stock':
-        [
-            '',
-        ],
-    'Capital Surplus':
-        [
-            '',
-        ],
-    'Shareholder Equity':
-        [
-            '',
-        ],
-    'Net Tangible Assets':
-        [
-            '',
-        ],
-    'Total Stockholders Equity':
-        [
-            '',
-        ],
-    'Net Cash Flow':
-        [
-            '',
-        ],
-    'Net Cash Flow-Operating':
-        [
-            '',
-        ],
-    'Net Cash Flows-Investing':
-        [
-            '',
-        ],
-    'Net Cash Flows-Financing':
-        [
-            '',
-        ],
-    'Effect of Exchange Rate Changes':
-        [
-            '',
-        ],
-    'Net Change in Cash':
-        [
-            '',
-        ],
-    'Cash Interest Paid':
-        [
-            '',
-        ],
-    'Cash Taxes Paid':
-        [
-            '',
-        ],
-    'Depreciation and Amortization':
-        [
-            '',
-        ],
-    'Capital Expenditures':
-        [
-            '',
-        ],
-    'Change in Working Capital':
-        [
-            '',
-        ],
-    'Free Cash Flow':
-        [
-            '',
-        ],
-    'Free Cash Flow per Share':
-        [
-            '',
-        ],
-    'Operating Cash Flow per Share':
-        [
-            '',
-        ],
-    'Cash per Share':
-        [
-            '',
-        ],
-    'Book Value per Share':
-        [
-            '',
-        ],
-    'Tangible Book Value per Share':
-        [
-            '',
-        ],
-    'Shareholders Equity per Share':
-        [
-            '',
-        ],
-    'Interest Debt per Share':
-        [
-            '',
-        ],
-    'Market Cap':
-        [
-            '',
-        ],
-    'Enterprise Value':
-        [
-            '',
-        ],
-    'PE Ratio':
-        [
-            '',
-        ],
-    'Price to Sales Ratio':
-        [
-            '',
-        ],
-    'POCF Ratio':
-        [
-            '',
-        ],
-    'PFCF Ratio':
-        [
-            '',
-        ],
-    'PB Ratio':
-        [
-            '',
-        ],
-    'PTB Ratio':
-        [
-            '',
-        ],
-    'EV to Sales':
-        [
-            '',
-        ],
-    'Enterprise Value over EBITDA':
-        [
-            '',
-        ],
-    'EV to Operating cash flow':
-        [
-            '',
-        ],
-    'Geographical':
-        [
-            'Statement Geographical [Axis]',
-            'Statement, Geographical [Axis]',
-            'Geographical [Axis]',
-        ],
-    'Product and Service':
-        [
-            'Product Or Service [Axis]',
-            'Products and Services [Axis]',
-            'Product and Service [Axis]',
-        ],
-    'Business Segment':
-        [
-            'Statement Business Segments [Axis]',
-            'Segment [Axis]',
-        ],
+    "Revenue": [
+        "Revenue from Contract with Customer, Excluding Assessed Tax",
+        "Revenues",
+        "Revenue, Net",
+        "Sales Revenue Net",
+        "Sales Revenue, Net",
+    ],
+    "Cost of Goods Sold": [
+        "Cost of Goods Sold",
+        "Cost Of Goods Sold",
+    ],
+    "Cost of Services": [
+        "Cost of Services",
+        "Cost Of Services",
+    ],
+    "Cost of Revenue": [
+        "Cost of sales",
+        "Cost of Revenue",
+        "Cost Of Revenue",
+        "Cost of Goods and Services Sold",
+        "Cost Of Goods And Services Sold",
+    ],
+    "Gross Profit": [
+        "",
+    ],
+    "Operating Expenses": [
+        "Operating Expenses",
+    ],
+    "Operating Income": [
+        "",
+    ],
+    "Interest Expense": [
+        "",
+    ],
+    "Income Before Tax": [
+        "",
+    ],
+    "Income Tax Expense": ["Income Taxes Paid, Net", "Income Taxes Paid Net"],
+    "Net Income": [
+        "Net Income (Loss)",
+        "Net income",
+        "Net sales",
+        "Net Income Loss",
+    ],
+    "Cash and Cash Equivalents": [
+        "",
+    ],
+    "Short Term Investments": [
+        "",
+    ],
+    "Total Cash": [
+        "",
+    ],
+    "Accounts Receivable": [
+        "Accounts Receivable Net Current",
+        "Accounts Receivable, Net, Current",
+    ],
+    "Accounts Payable": ["Accounts Payable Current", "Accounts Payable, Current"],
+    "Net Receivables": [
+        "",
+    ],
+    "Inventory": [
+        "",
+    ],
+    "Other Current Assets": [
+        "",
+    ],
+    "Total Current Assets": [
+        "",
+    ],
+    "Long Term Investments": [
+        "",
+    ],
+    "Property Plant and Equipment": [
+        "",
+    ],
+    "Goodwill": [
+        "",
+    ],
+    "Intangible Assets": [
+        "",
+    ],
+    "Other Assets": [
+        "",
+    ],
+    "Total Assets": [
+        "",
+    ],
+    "Short/Current Long Term Debt": [
+        "",
+    ],
+    "Other Current Liabilities": [
+        "",
+    ],
+    "Total Current Liabilities": [
+        "",
+    ],
+    "Long Term Debt": [
+        "",
+    ],
+    "Other Liabilities": [
+        "",
+    ],
+    "Total Liabilities": [
+        "",
+    ],
+    "Common Stock": [
+        "",
+    ],
+    "Retained Earnings": [
+        "",
+    ],
+    "Treasury Stock": [
+        "",
+    ],
+    "Capital Surplus": [
+        "",
+    ],
+    "Shareholder Equity": [
+        "",
+    ],
+    "Net Tangible Assets": [
+        "",
+    ],
+    "Total Stockholders Equity": [
+        "",
+    ],
+    "Net Cash Flow": [
+        "",
+    ],
+    "Net Cash Flow-Operating": [
+        "",
+    ],
+    "Net Cash Flows-Investing": [
+        "",
+    ],
+    "Net Cash Flows-Financing": [
+        "",
+    ],
+    "Effect of Exchange Rate Changes": [
+        "",
+    ],
+    "Net Change in Cash": [
+        "",
+    ],
+    "Cash Interest Paid": [
+        "",
+    ],
+    "Cash Taxes Paid": [
+        "",
+    ],
+    "Depreciation and Amortization": [
+        "",
+    ],
+    "Capital Expenditures": [
+        "",
+    ],
+    "Change in Working Capital": [
+        "",
+    ],
+    "Free Cash Flow": [
+        "",
+    ],
+    "Free Cash Flow per Share": [
+        "",
+    ],
+    "Operating Cash Flow per Share": [
+        "",
+    ],
+    "Cash per Share": [
+        "",
+    ],
+    "Book Value per Share": [
+        "",
+    ],
+    "Tangible Book Value per Share": [
+        "",
+    ],
+    "Shareholders Equity per Share": [
+        "",
+    ],
+    "Interest Debt per Share": [
+        "",
+    ],
+    "Market Cap": [
+        "",
+    ],
+    "Enterprise Value": [
+        "",
+    ],
+    "PE Ratio": [
+        "",
+    ],
+    "Price to Sales Ratio": [
+        "",
+    ],
+    "POCF Ratio": [
+        "",
+    ],
+    "PFCF Ratio": [
+        "",
+    ],
+    "PB Ratio": [
+        "",
+    ],
+    "PTB Ratio": [
+        "",
+    ],
+    "EV to Sales": [
+        "",
+    ],
+    "Enterprise Value over EBITDA": [
+        "",
+    ],
+    "EV to Operating cash flow": [
+        "",
+    ],
+    "Geographical": [
+        "Statement Geographical [Axis]",
+        "Statement, Geographical [Axis]",
+        "Geographical [Axis]",
+    ],
+    "Product and Service": [
+        "Product Or Service [Axis]",
+        "Products and Services [Axis]",
+        "Product and Service [Axis]",
+    ],
+    "Business Segment": [
+        "Statement Business Segments [Axis]",
+        "Segment [Axis]",
+    ],
 }
--- utils/database/_connector.py
+++ utils/database/_connector.py
@@ -1,19 +1,15 @@
-# Built-in libraries
-import datetime as dt
-from typing import List, Literal
-
 # Third party libraries
 from pymongo import MongoClient, ASCENDING
-from pymongo import IndexModel, UpdateOne
+from pymongo import IndexModel
 from pymongo.errors import OperationFailure
 
 # Internal imports
 from utils._logger import MyLogger
 
 
-class SECDatabase(MyLogger):
+class SECDatabase:
     def __init__(self, connection_string):
-        super().__init__(name="SECDatabase", level="DEBUG", log_file="././logs.log")
+        self.scrape_logger = MyLogger(name="MongoDB").scrape_logger
         self.client = MongoClient(connection_string)
         self.db = self.client.SECRawData
         self.tickerdata = self.db.TickerData
@@ -70,100 +66,3 @@ class SECDatabase(MyLogger):
             return self.tickerdata.find_one({"tickers": ticker.upper()})
         else:
             raise Exception("Please provide either a CIK or ticker.")
-
-    def insert_submission(self, submission: dict):
-        """Insert submissions into SEC database. CIK is the primary key.
-
-        Args:
-            ticker (TickerData): TickerData object
-
-        Returns:
-            str: empty string if successful
-            str: ticker's cik if failed
-        """
-        submission["lastUpdated"] = dt.datetime.now()
-        try:
-            self.tickerdata.update_one(
-                {"cik": submission["cik"]}, {"$set": submission}, upsert=True
-            )
-            self.scrape_logger.info(
-                f'Inserted submissions for {submission["cik"]} into SEC database.'
-            )
-
-        except Exception as e:
-            self.scrape_logger.error(
-                f'Failed to insert submissions for {submission["cik"]} into SEC database. Error: {e}'
-            )
-            return submission["cik"]
-        return None
-
-    def insert_filings(self, cik: str, filings: list):
-        """Insert filings into SEC database. Each submission has many filings. Accession number is the primary key.
-
-        Args:
-            ticker (TickerData): TickerData object
-
-        Returns:
-            str: empty string if successful
-            str: ticker's cik if failed
-        """
-        try:
-            for doc in filings:
-                doc["lastUpdated"] = dt.datetime.now()
-
-            update_requests = [
-                UpdateOne(
-                    {"accessionNumber": doc["accessionNumber"]},
-                    {"$set": doc},
-                    upsert=True,
-                )
-                for doc in filings
-            ]
-
-            self.tickerfilings.bulk_write(update_requests)
-            self.scrape_logger.info(f"Sucessfully updated filings for {cik}...")
-
-        except Exception as e:
-            self.scrape_logger.error(f"Failed to insert filings for {cik}...{e}")
-            return cik
-        return None
-
-    def create_update_request(
-        self,
-        accessionNumber: str,
-        items_label: Literal["facts", "labels", "context"],
-        items_dict: List[dict],
-    ):
-        update = UpdateOne(
-            {"accessionNumber": accessionNumber},
-            {"$set": {items_label: items_dict, "lastUpdated": dt.datetime.now()}},
-            upsert=True,
-        )
-        return update
-
-    def insert_facts(self, accession: str, facts: list):
-        """Insert facts into SEC database. Each filing has many facts.
-
-        Args:
-            facts (list): A list containing facts for a single filing
-
-        Returns:
-            str: empty string if successful
-            str: ticker's cik if failed
-        """
-        try:
-            for doc in facts:
-                doc["lastUpdated"] = dt.datetime.now()
-
-            fact_update_requests = [
-                UpdateOne({"factId": fact["factId"]}, {"$set": fact}, upsert=True)
-                for fact in facts
-            ]
-
-            self.factsdb.bulk_write(fact_update_requests)
-            self.scrape_logger.info(f"Updated facts for {accession}...")
-
-        except Exception as e:
-            self.scrape_logger.error(f"Failed to insert facts for {accession}...{e}")
-            return accession
-        return None
